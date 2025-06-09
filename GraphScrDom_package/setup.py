aacd8a71606ec518c0034ab57899cacd38cf14fd
Xin Ma <136360597+XinBiostats@users.noreply.github.com>
2025-06-09 14:25:02 -0700

    Update setup.py

--- GraphScrDom_package/setup.py
+++ GraphScrDom_package/setup.py
@@ -14,7 +14,7 @@ setup(
         'POT==0.9.4',
         'scanpy==1.9.6',
         'scikit-learn==1.3.2',
-        'scikit-misc==0.1.4',
+        #'scikit-misc==0.1.4',
         'scipy==1.14.1',
         'torch==1.13.1',
         'torch-geometric==2.5.2',
